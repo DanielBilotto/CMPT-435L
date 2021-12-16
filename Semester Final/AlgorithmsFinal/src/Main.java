e69fab4bdf372d36bb3d946b24bedbaf904d2c36
DanielBilotto <dbilotto01@yahoo.com>
2021-12-15 23:21:29 -0500

    second commit

--- Semester Final/AlgorithmsFinal/src/Main.java
+++ Semester Final/AlgorithmsFinal/src/Main.java
@@ -7,10 +7,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		int size = 1000;
+		double theSize = size;
 		double infectSize = size * .02;
 		boolean[] pool = new boolean[size];
 		int testNums = 0;
 		int count = 0;
+		double caseOne = 0;
+		double caseTwo = 0;
+		double caseThree = 0;
+		double onePer = 0;
+		double twoPer = 0;
+		double threePer = 0;
 		
 		
 		for (int i = 0; i < size; i++)
@@ -83,6 +90,7 @@ public class Main {
 					for (int o = 0; o < 4; o++)
 					{
 						testNums++;
+						caseTwo++;
 						//System.out.println("hi2");
 					}
 						
@@ -94,7 +102,16 @@ public class Main {
 					for (int t = 0; t < 4; t++)
 					{
 						testNums++;
+						caseTwo++;
 						//System.out.println("hi3");
+					}
+				}
+				
+				if (testOne.contains(true) && testTwo.contains(true))
+				{
+					for (int t = 0; t < 4; t++)
+					{
+						caseThree++;
 					}
 				}
 				
@@ -103,6 +120,7 @@ public class Main {
 			else
 			{
 				testNums++;
+				caseOne++;
 				//System.out.println("hi4");
 				
 			}
@@ -110,7 +128,19 @@ public class Main {
 			
 			
 		}
+		onePer = caseOne/theSize;
+		twoPer = caseTwo/theSize;
+		threePer = caseThree/theSize;
 		
+		System.out.println("Expected:");
+		System.out.println("Case 1: 125 x 0.8500 = 106.25 instances requiring 107 tests (since there are no partial tests)");
+		System.out.println("Case 2: 125 × 0.1496 = 18.70 instances requiring 131 tests");
+		System.out.println("Case 3: 125 × 0.0004 = 0.05 round up to 1 instance requiring 11 tests");
+		System.out.println("That's 249 tests to screen a population of 1000 people for a disease with an infection rate of 2%.");
+		System.out.println("Actual:");
+		System.out.println("Case 1: " + size/8 + " x " + onePer + " = " + ((size/3) * (onePer)) + " instances requiring " + caseOne + " tests (since there are no partial tests)");
+		System.out.println("Case 2: " + size/8 + " x " + twoPer + " = " + ((size/3) * (twoPer)) + " instances requiring " + caseTwo + " tests");
+		System.out.println("Case 3: " + size/8 + " x " + threePer + " = " + ((size/3) * (threePer)) + " instances requiring " + caseThree + " tests");
 		System.out.println("That's " + testNums + " tests to screen a population of " + size + " people for a disease with an infection rate of 2%.");
 	}//main
 	
